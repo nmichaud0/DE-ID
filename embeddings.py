d80585e76161007b9963c3b50f8577388bacfc65
nmichaud0 <nizar.michaud@gmail.com>
2025-10-01 09:16:17 +0200

    oct 1 commit

--- embeddings.py
+++ embeddings.py
@@ -1,26 +1,37 @@
 from transformers import AutoTokenizer, AutoModel
 import torch
+from sentence_transformers import SentenceTransformer
 
-def get_embeddings(strings: list[str], layer: int = -1) -> torch.tensor:
+def get_embeddings(strings: list[str], layer: int = -1, model_type: str = 'hf') -> torch.tensor:
 
-    tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-560m")
-    model = AutoModel.from_pretrained("bigscience/bloom-560m")
-
-    inputs = tokenizer(strings, return_tensors="pt", padding=True)
-
-    with torch.no_grad():
-        outputs = model(**inputs, output_hidden_states=True)
-
-    hidden_states = outputs.hidden_states[layer]
-
-    # last_hidden_state = outputs.last_hidden_state  # shape: [batch, sequence, hidden_size]
-
-    # To get a single embedding per sentence, you can use:
-    # [CLS] token if available, or mean pooling over all tokens
-    sentence_embedding = hidden_states.mean(dim=1)
+    assert model_type in ('hf', 'sbert'), f'model_type must be "hf" or "sbert", got {model_type}'
 
 
-    return sentence_embedding
+    if model_type == 'hf':
+        tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-560m")
+        model = AutoModel.from_pretrained("bigscience/bloom-560m")
+
+        inputs = tokenizer(strings, return_tensors="pt", padding=True)
+
+        with torch.no_grad():
+            outputs = model(**inputs, output_hidden_states=True)
+
+        hidden_states = outputs.hidden_states[layer]
+
+        # last_hidden_state = outputs.last_hidden_state  # shape: [batch, sequence, hidden_size]
+
+        # To get a single embedding per sentence, you can use:
+        # [CLS] token if available, or mean pooling over all tokens
+        sentence_embedding = hidden_states.mean(dim=1)
+
+        return sentence_embedding
+
+    else:
+
+        model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
+        embeddings = model.encode(strings)
+
+        return embeddings
 
 
 def get_roll_embeddings(strings: list[str], window_size: int = 3, layer: int = -1):
@@ -35,3 +46,6 @@ def get_roll_embeddings(strings: list[str], window_size: int = 3, layer: int = -
         roll_tokens.append(' '.join(strings[i:idx]))
 
     return get_embeddings(roll_tokens)
+
+if __name__ == '__main__':
+    x = get_embeddings(['Hi', 'this', 'is', 'a', 'test'], model_type='sbert')
